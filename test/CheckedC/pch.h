3ae4c6198d5ed5120d7e9422e3ca843853f7bb99
Archibald Samuel Elliott <t-arelli@microsoft.com>
2017-01-18 14:30:49 -0800

    Improves and adds additional PCH tests

--- test/CheckedC/pch.h
+++ test/CheckedC/pch.h
@@ -1,39 +1,78 @@
 // Used with the pch.c test
 
-// Bounds Expressions on globals
+//
+// Bounds Expressions on global variables
+//
 
 // CountBounds
 _Array_ptr<int> one_arr : count(1);
-typedef typeof(one_arr) one_element_array;
+
+// Byte Count
+_Array_ptr<int> byte_arr : byte_count(sizeof(int));
 
 // NullaryBounds
 _Array_ptr<int> null_arr : bounds(none);
-typedef typeof(null_arr) null_array;
 
 // RangeBounds
-int two_arr[2] = { 0, 0 };
+int two_arr[2];
 _Array_ptr<int> ranged_arr : bounds(&two_arr, &two_arr + 1);
-typedef typeof(ranged_arr) ranged_array;
 
 // InteropTypeBoundsAnnotation
 int* int_ptr : itype(_Ptr<int>);
-typedef typeof(int_ptr) integer_pointer;
 
-
+//
 // Bounds Expressions on functions
-int accepts_singleton(_Array_ptr<int> one_arr : count(1));
+//
+
+// CountBounds
+int count_fn(_Array_ptr<int> arr : count(1));
+_Array_ptr<int> count_fn2(void) : count(1);
+
+// Byte Count
+int byte_count_fn(_Array_ptr<int> arr : byte_count(sizeof(int)));
+_Array_ptr<int> byte_count_fn2(void) : byte_count(sizeof(int));
 
 // NullaryBounds
-int accepts_null(_Array_ptr<int> null_arr : bounds(none));
+int none_fn(_Array_ptr<int> null_arr : bounds(none));
+_Array_ptr<int> none_fn2(void) : bounds(none);
 
 // RangeBounds + PositionalParameter
-int sum_array(_Array_ptr<int> start : bounds(start, end) , _Array_ptr<int> end);
+int range_fn(_Array_ptr<int> start : bounds(start, end), _Array_ptr<int> end);
+_Array_ptr<int> range_fn2(_Array_ptr<int> start) : bounds(start, start);
 
-// PositionalParameter
-int str_last(int len, _Array_ptr<char> str : count(len));
+// CountBounds +  PositionalParameter
+int pos_fn(int len, _Array_ptr<char> str : count(len));
+_Array_ptr<int> pos_fn2(int len) : count(len);
 
 // InteropTypeBoundsAnnotation
 int int_val(int *ptr : itype(_Ptr<int>));
+int* int_val2(void) : itype(_Ptr<int>);
 
-// dropping bounds errors
-int accepts_pair(_Array_ptr<int> two_arr : count(2));
+//
+// Bounds Expressions on Struct Members
+//
+
+// CountBounds
+struct S1 {
+  _Array_ptr<int> arr : count(5);
+};
+
+// Byte Count
+struct S2 {
+  _Array_ptr<int> arr : byte_count(sizeof(int) * 5);
+};
+
+// NullaryBounds
+struct S3 {
+  _Array_ptr<int> arr : bounds(none);
+};
+
+// RangeBounds
+struct S4 {
+  _Array_ptr<long> arr : bounds(arr, arr + 5);
+};
+
+// InteropTypeBoundsAnnotation
+struct S5 {
+  int* i : itype(_Ptr<int>);
+};
