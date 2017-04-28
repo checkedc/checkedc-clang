f15b14019dbc4460f238c8dd73cb2dba5bc66490
wonsub kim <wonsub79.kim@samsung.com>
2017-04-28 14:11:46 +0900

    + Apply reviewer's feedback (#277)   + adjust error message   + bounds(any) check is done in code generation of dynamic check not static bounds checking

--- lib/Sema/SemaBounds.cpp
+++ lib/Sema/SemaBounds.cpp
@@ -937,7 +937,8 @@ namespace {
       // If inferred bounds of e1 are bounds(none), compile-time error.
       // If inferred bounds of e1 are bounds(any), no runtime checks.
       // Otherwise, the inferred bounds is bounds(lb, ub).
-      // In code geneartion, it inserts dynamic check(lb <= e2 && e3 <= ub)
+      // bounds of cast operation is bounds(e2, e3).
+      // In code geneartion, it inserts dynamic_check(lb <= e2 && e3 <= ub).
       if (CK == CK_DynamicPtrBounds) {
         BoundsExpr *SrcBounds = S.InferRValueBounds(E);
         Expr *subExpr = E->getSubExpr();
@@ -946,8 +947,6 @@ namespace {
         if (subExprBounds->isNone()) {
           S.Diag(subExpr->getLocStart(), diag::err_expected_bounds);
           SrcBounds = S.CreateInvalidBoundsExpr();
-        } else if (subExprBounds->isAny()) {
-          E->setCastKind(CK_AssumePtrBounds);
         }
 
         assert(SrcBounds);
