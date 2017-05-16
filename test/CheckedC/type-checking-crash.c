ac1ab8d1b43c8b44ebb771e3d0a40b30c2444b21
Jay Lim <nahk4r@gmail.com>
2017-05-16 10:08:03 -0700

    Update type-checking-crash.c
    I have changed the comments to look like:
    1. summary of this test
    2. blank line
    3. more description of why this test was written.

--- test/CheckedC/type-checking-crash.c
+++ test/CheckedC/type-checking-crash.c
@@ -1,12 +1,18 @@
-// Checks for crashes during type-checking error.
-// This was caused by an lvalue assert in SemaBounds.cpp
-// The goal of this test is to make sure we're seeing 
-// type-checking errors, but not get any crashes.
-// The llvm-lit test will fail the test if the compiler
-// crashes because of assert failure (This has been tested)
-// I wanted to use FileCheck to specifically look for
-// "assert failed" output but for some reason FileCheck 
-// refuses to work. So I had to use this method.
+// Tests to make sure the checked c code does not crash the compiler
+// when it encounters an incorrectly constructed AST caused by type
+// checking error.
+//
+// More specifically, take a look at the four test cases below.
+// First of all, even when clang encounters a type checking error,
+// it does its best to create the entire AST, before it exits with
+// an error message.
+// struct_int_array_type_checking_crash_test_1 creates an invalid AST,
+// where "arr" is not an lvalue, although it should be an lvalue.
+// Interestingly, for the other three test cases, the constructed AST
+// has "arr" to be an lvalue.
+// This test makes sure that no matter the case, the checked c code will
+// not exit the program with an exception.
+//
 // RUN: %clang_cc1 -fcheckedc-extension -verify %s
 
 struct S {
@@ -14,22 +20,38 @@ struct S {
 	int data;
 };
 
+//====================================================================
+// Accessing struct with "->" causes error and wrongly eliminates lvalue
+//====================================================================
+
 void struct_int_array_type_checking_crash_test_1(int i) {
 	struct S s = { { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 10 };
-	s->arr[i] = 1; // expected-error{{expression has no bounds}}	// expected-error{{member reference type 'struct S' is not a pointer; did you mean to use '.'?}} 
+	s->arr[i] = 1; //expected-error{{member reference type 'struct S' is not a pointer; did you mean to use '.'?}} //expected-error{{expression has no bounds}}
 }
+
+//====================================================================
+// Correctly accessing struct.
+//====================================================================
 
 void struct_int_array_type_checking_crash_test_2(int i) {
 	struct S s = { { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 10 };
 	s.arr[i] = 1;
 }
 
+//====================================================================
+// Correctly accessing struct pointer.
+//====================================================================
+
 void struct_int_array_type_checking_crash_test_3(int i) {
 	struct S s = { { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 10 };
 	(&s)->arr[i] = 1;
 }
 
+//====================================================================
+// Accessing struct pointer with "." causes error but keeps lvalue
+//====================================================================
+
 void struct_int_array_type_checking_crash_test_4(int i) {
 	struct S s = { { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 10 };
-	(&s).arr[i] = 1;// expected-error{{member reference type 'struct S *' is a pointer; did you mean to use '->'?}}
+	(&s).arr[i] = 1;//expected-error{{member reference type 'struct S *' is a pointer; did you mean to use '->'?}}
 }
