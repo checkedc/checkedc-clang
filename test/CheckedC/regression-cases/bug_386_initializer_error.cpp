5b176ea5fc9a9d63b079dedec3829d52a23de52b
David Tarditi <dtarditi@microsoft.com>
2017-09-29 09:32:20 -0700

    Duplicate error message when initializer has an error.
    When an initializer has a syntax or typechecking error, it is not added to the
    AST.  This causes the compiler to complain during static checking of bounds
    about the lack of an initializer.   The fix is to issue the error message
    earlier.
    
    Testing:
    - Added a regression test case that checks that only one diagnostic message
      about the error in the initializer is emitted.

---
+++ test/CheckedC/regression-cases/bug_386_initializer_error.cpp
@@ -0,0 +1,14 @@
+//
+// These is a regression test case for 
+// https://github.com/Microsoft/checkedc-clang/issues/386
+//
+// The test check that there are not duplicate error messages when an initializer
+// has a syntax or typechecking error.  The compiler was issue a message about 
+// a checked variable needed an initializer in that case.
+//
+// RUN: %clang -cc1 -verify -fcheckedc-extension %s
+
+void f(void) {
+  int i = 0;
+  array_ptr<int> p : count(1) = i + var;  // expected-error {{use of undeclared identifier 'var'}}
+}
