33f3ec655f020f7a0f84a6bd8198662963aee1d2
David Tarditi <dtarditi@microsoft.com>
2017-11-29 14:55:24 -0500

    Update error message to use the term "unknown bounds". (#430)
    We changed the language to use bounds(unknown) instead of bounds(none)
    because bounds(none) was ambiguous.  Make a similar update to compiler error
    messages, changing from using the phrase "no bounds" to "unknown bounds."
    
    Also fix line endings for test\CheckedC\type-checking-crash.c.
    
    Testing
    - Updated error messages in the Checked C repo tests.
    - Passed automated testing.

--- test/CheckedC/regression-cases/ptr_cast_bug_242.c
+++ test/CheckedC/regression-cases/ptr_cast_bug_242.c
@@ -10,16 +10,16 @@
 // Test explicit cast.
 void f(int *p) {
   // Test explicit cast where bounds are expected for the entire initializing expression.
-  _Array_ptr<int> q : count(1) = (_Ptr<int>)(p); // expected-error {{expression has no bounds, cast to ptr<T> expects source to have bounds}}
+  _Array_ptr<int> q : count(1) = (_Ptr<int>)(p); // expected-error {{expression has unknown bounds, cast to ptr<T> expects source to have bounds}}
   // Test explicit cast.
-  _Ptr<int> r = (_Ptr<int>)(p);    // expected-error {{expression has no bounds, cast to ptr<T> expects source to have bounds}}
+  _Ptr<int> r = (_Ptr<int>)(p);    // expected-error {{expression has unknown bounds, cast to ptr<T> expects source to have bounds}}
   // Test implicit cast.
-  _Ptr<int> s = p;                 // expected-error {{expression has no bounds, cast to ptr<T> expects source to have bounds}}
+  _Ptr<int> s = p;                 // expected-error {{expression has unknown bounds, cast to ptr<T> expects source to have bounds}}
   _Array_ptr<int> t : count(1) = 0;
   // Test explicit cast involving assignment;
-  t = (_Ptr<int>)(p);              // expected-error {{expression has no bounds, cast to ptr<T> expects source to have bounds}}
+  t = (_Ptr<int>)(p);              // expected-error {{expression has unknown bounds, cast to ptr<T> expects source to have bounds}}
   _Ptr<int> u = 0;
   // Test implicit cast involving assignment.
-  u = p;                           // expected-error {{expression has no bounds, cast to ptr<T> expects source to have bounds}}
+  u = p;                           // expected-error {{expression has unknown bounds, cast to ptr<T> expects source to have bounds}}
 }
 
