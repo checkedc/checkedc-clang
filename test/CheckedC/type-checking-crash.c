28935019d61f21f2e1ea5ea44b7904e4ca36ff12
David Tarditi <dtarditi@microsoft.com>
2017-11-29 11:03:36 -0800

    Update error message to use the term "unknown bounds".
    We changed the compiler to use bounds(unknown) instead of bounds(none)
    because bounds(none) was ambiguous.  Make a similar update to error
    messages, changing from using the phrase "no bounds" to "unknown bounds."
    
    Also fix line endings for test\CheckedC\type-checking-crash.c

--- test/CheckedC/type-checking-crash.c
+++ test/CheckedC/type-checking-crash.c
@@ -1,13 +1,13 @@
-// Tests to make sure the Checked C code does not crash the compiler when it 
+// Tests to make sure the Checked C code does not crash the compiler when it
 // encounters an incorrectly constructed AST caused by type checking error.
 //
-// More specifically, take a look at the four test cases below. First of all, 
-// even when clang encounters a type checking error, it does its best to create 
-// the entire AST, before it exits with an error message. 
-// Struct_int_array_type_checking_crash_test_1 creates an invalid AST, where 
-// "arr" is not an lvalue, although it should be an lvalue. Interestingly, for 
-// the other three test cases, the constructed AST has "arr" to be an lvalue. 
-// This test makes sure that no matter the case, the Checked C code will not 
+// More specifically, take a look at the four test cases below. First of all,
+// even when clang encounters a type checking error, it does its best to create
+// the entire AST, before it exits with an error message.
+// Struct_int_array_type_checking_crash_test_1 creates an invalid AST, where
+// "arr" is not an lvalue, although it should be an lvalue. Interestingly, for
+// the other three test cases, the constructed AST has "arr" to be an lvalue.
+// This test makes sure that no matter the case, the Checked C code will not
 // exit the program with an exception.
 //
 // RUN: %clang_cc1 -fcheckedc-extension -verify %s
@@ -23,7 +23,7 @@ struct S {
 
 void struct_int_array_type_checking_crash_test_1(int i) {
   struct S s = { { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 10 };
-  s->arr[i] = 1; //expected-error{{member reference type 'struct S' is not a pointer; did you mean to use '.'?}} //expected-error{{expression has no bounds}}
+  s->arr[i] = 1; //expected-error{{member reference type 'struct S' is not a pointer; did you mean to use '.'?}} //expected-error{{expression has unknown bounds}}
 }
 
 //====================================================================
