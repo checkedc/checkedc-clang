aee4ff4b3dcef2792501af9b80803e44eca77d1d
Anna Kornfeld Simpson <t-ansimp@microsoft.com>
2018-06-13 17:39:02 -0700

    Progress acquiring cmake options

--- automation/UNIX/build-package.sh
+++ automation/UNIX/build-package.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+
+function resetBeforeExit {
+  set +ue
+  set +o pipefail
+}
+
+function cmdsucceeded {
+  resetBeforeExit
+  exit 0
+}
+
 function cmdfailed {
   echo "Build installation package failed."
+  resetBeforeExit
   exit 1
 }
+
+set -ue
+set -o pipefail
+set -x
+
+cd ${LLVM_OBJ_DIR}
+
+if [ "${BUILD_PACKAGE}" == "No" ]; then cmdsucceeded; fi
+
+echo "Building installation package for clang"
+
+#cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON 
