5c985c9b331240be02535a97e86392b395356490
Anna Kornfeld Simpson <t-ansimp@microsoft.com>
2018-08-03 10:07:55 -0700

    Remove the spurious target_arch x86 if statement from run-cmake.sh

--- automation/UNIX/run-cmake.sh
+++ automation/UNIX/run-cmake.sh
@@ -6,12 +6,6 @@ set -ue
 set -o pipefail
 set -x
 
-if [ "${TEST_TARGET_ARCH}" == "X86" ]; then
-  CMAKE_ADDITIONAL_OPTIONS="-DLLVM_TARGET_ARCH=X86"
-else
-  CMAKE_ADDITIONAL_OPTIONS=""
-fi
-
 if [[ "$LNT" != "" && ("$BUILDCONFIGURATION" == "Release" || "$BUILDCONFIGURATION" == "ReleaseWithDebInfo") ]]; then
   CMAKE_ADDITIONAL_OPTIONS="$CMAKE_ADDITIONAL_OPTIONS -DLLVM_ENABLE_ASSERTIONS=On"
 fi
