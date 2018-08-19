036dcf9d9855005872126fbb707c4f6912afb963
David Tarditi <dtarditi@microsoft.com>
2018-08-19 00:26:03 -0700

    Disable additional test.

--- test/CheckedC/static-checking/bounds-decl-checking.c
+++ test/CheckedC/static-checking/bounds-decl-checking.c
@@ -321,9 +321,11 @@ _Array_ptr<int> f35(unsigned num) : count(num) {
 
 _Array_ptr<void> test_f34(unsigned size) : bounds((_Array_ptr<char>) _Return_value, (_Array_ptr<char>) _Return_value + size);
 
+#if 0
 _Array_ptr<int> f36(unsigned num) : count(num) {
-  _Array_ptr<int> p : count(num) = test_f34(num * sizeof(int));  // expected-warning {{cannot prove declared bounds for 'p' are valid after initialization}} \
-                                                                 // expected-note {{(expanded) declared bounds are 'bounds(p, p + num)'}} \
-                                                                 // expected-note {{(expanded) inferred bounds are 'bounds((_Array_ptr<char>)_Return_value, (_Array_ptr<char>)_Return_value + num * sizeof(int))'}}
+  _Array_ptr<int> p : count(num) = test_f34(num * sizeof(int));  // dummy-expected-warning {{cannot prove declared bounds for 'p' are valid after initialization}} \
+                                                                 // dummy-expected-note {{(expanded) declared bounds are 'bounds(p, p + num)'}} \
+                                                                 // dummy-expected-note {{(expanded) inferred bounds are 'bounds((_Array_ptr<char>)_Return_value, (_Array_ptr<char>)_Return_value + num * sizeof(int))'}}
   return p;
 }
+#endif
