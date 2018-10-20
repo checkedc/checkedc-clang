6848929e4ddb181e855ab8fb3a8696e955f31bf4
David Tarditi <dtarditi@microsoft.com>
2018-10-20 09:54:22 -0700

    Clean up regression test case.

--- test/CheckedC/regression-cases/bug_484_rewrite_parameter_uses.c
+++ test/CheckedC/regression-cases/bug_484_rewrite_parameter_uses.c
@@ -6,12 +6,13 @@
 // be fully checked. We need to rewrite declarations of parameters and uses of
 // parameters that have bounds-safe interfaces.
 //
-// The following example illustrates the problem, if wed don't do the rewrite.
+// The following example illustrates the problem, if we don't do the rewrite.
 // We define 3 C typedefs:
 // 1. The first one defines an unchecked function pointer with a bounds-safe
 // interface on a parameter.
-// 2. The second one defines the bounds-safe interface type the first one.
-// 3. The third one defines a full checked version.
+// 2. The second one defines the bounds-safe interface type for the first
+// typedef's function pointer type.
+// 3. The third one defines a fully checked version.
 //
 // If we don't rewrite the uses of parameters, the function types
 // don't match and we get a type checking error.
@@ -24,8 +25,6 @@ typedef  _Ptr<int (int *a : bounds(a, a + n), int n)> bsi_callback_fn3;
 typedef  _Ptr<int (_Array_ptr<int> a : bounds(a, a + n), int n)> checked_callback_fn3;
 
 _Checked callback_fn3 return_function_pointer(void) : itype(bsi_callback_fn3);
-
-// _Checked callback_fn3 return_function_pointer(void) : itype(_Ptr<int (int *a : bounds(a, a + n), int n)>);
 
 _Checked void test_function_pointer_return(void) {
    checked_callback_fn3 fn = return_function_pointer();
