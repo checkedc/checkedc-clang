8b2bc5ba7f9af965ab7f41f0820d58b26ca4713e
Jie Zhou <jzhou41@cs.rochester.edu>
2019-07-09 17:16:25 -0700

    Cleaned up the code that disallows casting to nt_array_ptr.
    In the code that disallows casting to nt_array_ptr in a checked scope,
    we can use an exsiting helper function isCheckedPointerNtArrayType()
    to test whether a pointer type is nt_array_ptr. This makes the code
    a little bit cleaner.

--- lib/Sema/SemaCast.cpp
+++ lib/Sema/SemaCast.cpp
@@ -2703,17 +2703,14 @@ void CastOperation::CheckCStyleCast(bool IsCheckedScope) {
 
     // Disallow cast from other Checked Pointer types to nt_arary_ptr because 
     // the SrcType might not point to a NULL-terminated array.
-    if (DestType->isPointerType()) {
-      if (cast<PointerType>(DestType)->getKind() == CheckedPointerKind::NtArray) {
-        if (SrcType->isPointerType() &&
-            cast<PointerType>(SrcType)->getKind() != CheckedPointerKind::NtArray) {
+    if (DestType->isPointerType() && DestType->isCheckedPointerNtArrayType()) {
+        if (SrcType->isPointerType() && !SrcType->isCheckedPointerNtArrayType()) {
           Self.Diag(SrcExpr.get()->getExprLoc(),
               diag::err_checked_scope_no_cast_to_nt_array_ptr)
             << SrcType << DestType << SrcExpr.get()->getSourceRange();
           SrcExpr = ExprError();
           return;
         }
-      } 
     }
 
   }
