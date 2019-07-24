61e4527fab561406c521704235880cf1ab9f7bc2
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2019-07-24 14:00:30 -0700

    Add tests for improvement of pointer arithmetic equivalence

--- test/CheckedC/static-checking/bounds-decl-checking-cs.c
+++ test/CheckedC/static-checking/bounds-decl-checking-cs.c
@@ -2,9 +2,8 @@
 // produces warnings about bounds declarations that are not provably
 // true or false.
 //
-// RUN: %clang -cc1 -fcheckedc-extension -verify %s
+// RUN: %clang_cc1 -fcheckedc-extension -verify %s
 
 #pragma CHECKED_SCOPE ON
 
 #include "bounds-decl-checking.c"
-
