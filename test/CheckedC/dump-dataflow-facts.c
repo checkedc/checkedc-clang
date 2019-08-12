4bc1b1da30f276ccdca56e755b214d972c8b3791
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2019-08-12 14:57:54 -0700

    Refactor

--- test/CheckedC/dump-dataflow-facts.c
+++ test/CheckedC/dump-dataflow-facts.c
@@ -1,6 +1,6 @@
 // Tests for dumping of datafow analysis for collecting facts
 //
-// RUN: %clang_cc1 -fcheckedc-extension -fdump-extracted-comparison-facts %s 2>1 | FileCheck %s
+// RUN: %clang_cc1 -fdump-extracted-comparison-facts %s 2>1 | FileCheck %s
 
 //===================================================================
 // Dumps of different kinds of collected dataflow facts
@@ -8,7 +8,6 @@
 
 
 int f(int a);
-
 void fn_1(void) {
   int a, b, c;
   if (b < c)
@@ -17,25 +16,25 @@ void fn_1(void) {
 }
 
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In: (b, c),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (b, c),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In: (b, c),
-// CHECK: Kill: (b, c), (c, b),
-// CHECK: }
+// CHECK-NEXT: In: (b, c),
+// CHECK-NEXT: Kill: (b, c), (c, b),
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 
 void fn_2(void) {
   int e1, e2, a, b, c, q, n;
@@ -52,41 +51,41 @@ void fn_2(void) {
 }
 
 // CHECK: Block #8: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #7: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #6: {
-// CHECK: In: (e1, e2),
-// CHECK: Kill: (b, c), (c, b),
-// CHECK: }
+// CHECK-NEXT: In: (e1, e2),
+// CHECK-NEXT: Kill: (b, c), (c, b),
+// CHECK-NEXT: }
 // CHECK: Block #5: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In: (c, b),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (c, b),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In: (b, c),
-// CHECK: Kill: (b, c), (c, b),
-// CHECK: }
+// CHECK-NEXT: In: (b, c),
+// CHECK-NEXT: Kill: (b, c), (c, b),
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 
 void fn_3(void) {
   int a, b, c;
@@ -95,33 +94,33 @@ void fn_3(void) {
 }
 
 // CHECK: Block #6: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #5: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 
 
 void fn_4(void) {
@@ -133,29 +132,29 @@ void fn_4(void) {
 }
 
 // CHECK: Block #5: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 
 _Nt_array_ptr<int> g(int a) : byte_count(a);
 _Nt_array_ptr<int> fn_5(int a) {
@@ -168,25 +167,25 @@ _Nt_array_ptr<int> fn_5(int a) {
 }
 
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In: (d, a),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (d, a),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In: (a, d),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (a, d),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 
 _Nt_array_ptr<int> fn_6(int a) {
   int d;
@@ -198,25 +197,25 @@ _Nt_array_ptr<int> fn_6(int a) {
 }
 
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In: (a, d),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (a, d),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In: (d, a),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (d, a),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 
 void fn_7(void) {
   int a, b, c;
@@ -227,30 +226,30 @@ void fn_7(void) {
 }
 
 // CHECK: Block #6: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #5: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #4: {
-// CHECK: In:
-// CHECK: Kill: (a, b), (b, a),
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill: (a, b), (b, a),
+// CHECK-NEXT: }
 // CHECK: Block #0: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #3: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #2: {
-// CHECK: In: (a, b),
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In: (a, b),
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
 // CHECK: Block #1: {
-// CHECK: In:
-// CHECK: Kill:
-// CHECK: }
+// CHECK-NEXT: In:
+// CHECK-NEXT: Kill:
+// CHECK-NEXT: }
