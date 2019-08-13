77d885c440cfb189004995d965f9fc92f513b9ce
Mandeep Singh Grang <magrang@microsoft.com>
2019-08-13 10:45:39 -0700

    Use computed value instead of re-emitting subexpr

--- test/CheckedC/dynamic-checks/ptr-arithmetic-null-checks-code-gen.c
+++ test/CheckedC/dynamic-checks/ptr-arithmetic-null-checks-code-gen.c
@@ -4,57 +4,109 @@
 #include <stdchecked.h>
 #include <stddef.h>
 
+array_ptr<char> p : count(3) = "abc";
+array_ptr<char> q : count(3) = "abc";
+// NOCHECK-NOT: _Dynamic_check.non_null
+
 void f1() {
 // CHECK-LABEL: f1
-  array_ptr<char> p = "abc";
-
-// NOCHECK-NOT: _Dynamic_check
-
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   p++;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f2() {
+// CHECK-LABEL: f2
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   ++p;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f3() {
+// CHECK-LABEL: f3
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   p--;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f4() {
+// CHECK-LABEL: f4
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   --p;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f5() {
+// CHECK-LABEL: f5
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   p += 5;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f6() {
+// CHECK-LABEL: f6
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   p -= 10;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f7() {
+// CHECK-LABEL: f7
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   p = 2 + p;
+}
 
-// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** %p, align 8
-// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+void f8() {
+// CHECK-LABEL: f8
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
 // CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
   p = p - 100;
+}
+
+void f9() {
+// CHECK-LABEL: f9
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
+// CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
+  p[1]++;
+}
+
+void f10() {
+// CHECK-LABEL: f10
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
+// CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
+
+// CHECK:      [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK:      [[REG2:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG2]], null
+// CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
+  p[1] = p[2] + (*p)++;
+}
+
+void f11(array_ptr<char> a);
+void f12() {
+// CHECK-LABEL: f12
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG0]], null
+// CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
+  f11(++p);
+}
+
+void f13() {
+// CHECK-LABEL: f13
+// CHECK:      [[REG0:%[a-zA-Z0-9.]*]] = load i8*, i8** @p, align 8
+// CHECK:      [[REG1:%[a-zA-Z0-9.]*]] = load i8*, i8** @q, align 8
+// CHECK-NEXT: [[REG_DYNN:%_Dynamic_check.non_null[a-zA-Z0-9.]*]] = icmp ne i8* [[REG1]], null
+// CHECK-NEXT: br i1 [[REG_DYNN]], label %[[LAB_DYSUC:_Dynamic_check.succeeded[a-zA-Z0-9.]*]], label %[[LAB_DYFAIL:_Dynamic_check.failed[a-zA-Z0-9.]*]]
+  p[q[1]++]++;
 }
