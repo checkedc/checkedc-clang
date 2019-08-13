6e35e70e3dada7c60d0168c1efd4c97930ce2dc6
Mandeep Singh Grang <magrang@microsoft.com>
2019-08-13 16:20:32 -0700

    Add comments

--- automation/UNIX/run-lnt-local.sh
+++ automation/UNIX/run-lnt-local.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# This script can be used to run LNT tests locally on a user's Linux machine.
+# For detailed setup instructions see
+# https://github.com/Microsoft/checkedc-llvm-test-suite/blob/master/README.md
+
+usage() {
+  echo "Usage: SRC_DIR=</path/to/llvm/src> BUILD_DIR=</path/to/llvm/build> $0"
+  echo "Optional flags: TEST_TARGET=\"X86_64;ARM\" LNT_BIN=</path/to/lnt>"
+  exit 1
+}
+
 CURDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [ -z "$SRC_DIR" ]; then
   echo "Specify SRC_DIR as the dir containing LLVM sources."
-  exit 1
+  usage
 fi
 
 if [ -z "$BUILD_DIR" ]; then
   echo "Specify BUILD_DIR as the dir containing LLVM build."
-  exit 1
+  usage
 fi
 
 if [ -z "$TEST_TARGET" ]; then
