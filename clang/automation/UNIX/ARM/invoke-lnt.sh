022867246a7399fa3221fe048598e277ea4431de
Mandeep Singh Grang <magrang@microsoft.com>
2019-09-10 17:15:12 -0700

    Simplify lnt invocation (#674)
    Cherry-picked from commit 48c39595fb0eaa6762dd47abe1a49919fa92acd9

--- clang/automation/UNIX/ARM/invoke-lnt.sh
+++ clang/automation/UNIX/ARM/invoke-lnt.sh
@@ -14,16 +14,14 @@ RUN=qemu-arm
 "$LNT_SCRIPT" runtest nt \
   -v \
   --sandbox "$RESULTS_DIR" \
-  --no-timestamp \
   --cc "$CC" \
   --cxx "$CXX" \
   --cflags "$CFLAGS" \
   --qemu-user-mode "$RUN" \
   --test-suite ${BUILD_SOURCESDIRECTORY}/llvm-test-suite \
-  --make-param=ExtraHeaders=${BUILD_SOURCESDIRECTORY}/llvm/projects/checkedc-wrapper/checkedc/include \
   --output=${RESULT_DATA} \
   -j${BUILD_CPU_COUNT} \
-  | tee ${RESULT_SUMMARY}
+  2>&1 | tee ${RESULT_SUMMARY}
 
 set +ue
 set +o pipefail
--- clang/automation/UNIX/X86_64/invoke-lnt.sh
+++ clang/automation/UNIX/X86_64/invoke-lnt.sh
@@ -11,15 +11,13 @@ CFLAGS="-fcheckedc-extension"
 "$LNT_SCRIPT" runtest nt \
   -v \
   --sandbox "$RESULTS_DIR" \
-  --no-timestamp \
   --cc "$CC" \
   --cxx "$CXX" \
   --cflags "$CFLAGS" \
   --test-suite ${BUILD_SOURCESDIRECTORY}/llvm-test-suite \
-  --make-param=ExtraHeaders=${BUILD_SOURCESDIRECTORY}/llvm/projects/checkedc-wrapper/checkedc/include \
   --output=${RESULT_DATA} \
   -j${BUILD_CPU_COUNT} \
-  | tee ${RESULT_SUMMARY}
+  2>&1 | tee ${RESULT_SUMMARY}
 
 set +ue
 set +o pipefail
