a5522831f2d3ad58571a0179793ae1614ec15126
Mandeep Singh Grang <magrang@microsoft.com>
2019-09-10 17:15:05 -0700

    Fix unbound variable RUN_LOCAL (#665)
    Cherry-picked from commit c6a1752f16bf2dbb5ac793ab2e154634247d09b5

--- clang/automation/UNIX/build-and-test.sh
+++ clang/automation/UNIX/build-and-test.sh
@@ -13,7 +13,7 @@ fi
 set -ue
 set -o pipefail
 
-if [ -z "$RUN_LOCAL" ]; then
+if [ "$RUN_LOCAL" = "no" ]; then
   ./setup-files.sh
   ./run-cmake.sh
   ./test-clang.sh
@@ -21,6 +21,6 @@ fi
 
 ./test-lnt.sh
 
-if [ -z "$RUN_LOCAL" ]; then
+if [ "$RUN_LOCAL" = "no" ]; then
   ./build-package.sh
 fi
