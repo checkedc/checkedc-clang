ce6eacc1960ef0501cc828be36f15d2604ff103f
Mandeep Singh Grang <magrang@microsoft.com>
2019-10-08 09:54:48 -0700

    Add benchmarking support for Checked C (#686)
    Cherry-picked from commit 2032050c5811801ee0b794b5e7d5937a421a6041
    Author: Mandeep Singh Grang <magrang@microsoft.com>
    Commit: David Tarditi <dtarditi@microsoft.com>
    
        Add benchmarking support for Checked C (#686)
    
        Update automation scripts for invoking LNT to support invoking LNT in a benchmarking mode.

--- clang/automation/UNIX/X86_64/invoke-lnt.sh
+++ clang/automation/UNIX/X86_64/invoke-lnt.sh
@@ -8,16 +8,33 @@ CC=clang
 CXX=${CC}++
 CFLAGS="-fcheckedc-extension"
 
-"$LNT_SCRIPT" runtest nt \
-  -v \
-  --sandbox "$RESULTS_DIR" \
-  --cc "$CC" \
-  --cxx "$CXX" \
-  --cflags "$CFLAGS" \
-  --test-suite ${BUILD_SOURCESDIRECTORY}/llvm-test-suite \
-  --output=${RESULT_DATA} \
-  -j${BUILD_CPU_COUNT} \
-  2>&1 | tee ${RESULT_SUMMARY}
+if [[ "$BMARK" = "yes" ]]; then
+  "$LNT_SCRIPT" runtest test-suite \
+    -v \
+    --sandbox "$RESULTS_DIR" \
+    --cc "$CC" \
+    --cxx "$CXX" \
+    --cflags "$CFLAGS" \
+    --test-suite "$BUILD_SOURCESDIRECTORY/llvm-test-suite" \
+    --submit "$LNT_DB_DIR" \
+    --only-test "$ONLY_TEST" \
+    --exec-multisample "$SAMPLES" \
+    --run-order "$USER" \
+    ${EXTRA_LNT_ARGS} \
+    2>&1 | tee $RESULT_SUMMARY
+
+else
+  "$LNT_SCRIPT" runtest nt \
+    -v \
+    --sandbox "$RESULTS_DIR" \
+    --cc "$CC" \
+    --cxx "$CXX" \
+    --cflags "$CFLAGS" \
+    --test-suite "$BUILD_SOURCESDIRECTORY/llvm-test-suite" \
+    --output "$RESULT_DATA" \
+    -j${BUILD_CPU_COUNT} \
+    2>&1 | tee $RESULT_SUMMARY
+fi
 
 set +ue
 set +o pipefail
