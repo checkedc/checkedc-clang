a756721dd84bfc1a316bd0184780fa415adc7b2d
Katherine Kjeer <6687333+kkjeer@users.noreply.github.com>
2019-11-06 12:27:59 -0800

    Add checked information for compound statements to AST reading/writing (#716)
    * Add compound block checked information to ASTWriterStmt and ASTReaderStmt
    
    * Add tests for pch checked scope AST

---
+++ clang/test/CheckedC/ast-dump-pch.c
@@ -0,0 +1,37 @@
+// Tests for serialization/deserialization of checked scopes.
+// This makes sure AST serialization/deserialization accounts for
+// checked scope specifiers and written checked scope specifiers.
+// 
+// RUN: %clang_cc1 -fcheckedc-extension -emit-pch -o %t %S/ast-dump-pch.h
+// RUN: %clang_cc1 -fcheckedc-extension -ast-dump-all -include-pch %t %s | FileCheck %s
+
+// CHECK: FunctionDecl
+// CHECK: f1
+// CHECK-NEXT: CompoundStmt
+// CHECK-NOT: {{_Checked|_Unchecked|checking-state}}
+// CHECK-NEXT: CompoundStmt
+// CHECK: _Checked checking-state bounds-and-types
+// CHECK: CompoundStmt
+// CHECK: _Checked _Bounds_only checking-state bounds
+// CHECK: CompoundStmt
+// CHECK: _Unchecked
+// CHECK: CompoundStmt
+// CHECK-NOT: {{_Checked|_Unchecked|checking-state}}
+
+// CHECK-NEXT: FunctionDecl
+// CHECK: f2
+// CHECK-NEXT: CompoundStmt
+// CHECK: _Checked checking-state bounds-and-types
+
+// CHECK-NEXT: FunctionDecl
+// CHECK: f3
+// CHECK-NEXT: CompoundStmt
+// CHECK: _Checked _Bounds_only checking-state bounds
+
+// CHECK-NEXT: FunctionDecl
+// CHECK: f4
+// CHECK-NEXT: CompoundStmt
+// CHECK: _Unchecked
+// CHECK-NOT {{checking-state}}
+
+// TODO: GitHub issue #704: add function declarations with checked scope specifiers
