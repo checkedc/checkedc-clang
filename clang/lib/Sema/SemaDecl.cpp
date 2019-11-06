17b7f3c34de36c88ed76324ac9db5e26f5cbabb0
kakje <kakje@microsoft.com>
2019-11-06 10:22:41 -0800

    Update error and note messages

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -7755,14 +7755,15 @@ void Sema::CheckVariableDeclarationType(VarDecl *NewVD) {
   if (T->isUndeducedType())
     return;
   
-  // Emit an error for each use of a free type variable in the declaration of a static variable.
+  // Emit an error for each use of a free type variable (type variable
+  // bound by an enclosing scope) in the declaration of a static variable.
   if (NewVD->getStorageClass() == StorageClass::SC_Static) {
     std::vector<const TypedefNameDecl *> FreeVarDecls = FindFreeVariableDecls(T);
     if (FreeVarDecls.size() > 0) {
       for (auto it = FreeVarDecls.begin(); it != FreeVarDecls.end(); ++it) {
         const TypedefNameDecl *FreeVar = *it;
         Diag(NewVD->getLocation(), diag::err_static_decl_uses_free_type_variable) 
-          << FreeVar->getName() << NewVD->getName();
+          << NewVD->getName() << FreeVar->getName();
         Diag(FreeVar->getLocation(), diag::note_free_type_variable_declared) << FreeVar->getName();
       }
       NewVD->setInvalidDecl();
