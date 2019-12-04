9e4b082838f9eddb2dc440ea059d52f4703cb825
Saeed Nejati <nejati@gmail.com>
2019-12-03 16:45:16 -0800

    Adding Seahorn verification tests

--- clang/test/CheckedC/verification/seahorn-checked.c
+++ clang/test/CheckedC/verification/seahorn-checked.c
@@ -1,8 +1,12 @@
-// Test for CodeGen when injecting verifier bad states
-// There should be a __VERIFIER_error at each dynamic check
+// Verification of checked codes using Seahorn
 //
+// REQUIRES: seahorn
 //
-// RUN: %clang -finject-verifier-calls -emit-llvm -S %s -o - | FileCheck %s
+// RUN: %clang -finject-verifier-calls -funchecked-pointers-dynamic-check -c -emit-llvm -O0 -m64 %s -o %t1
+// RUN: %sea_pp %t1 -o %t2
+// RUN: %sea_ms %t2 -o %t3
+// RUN: %sea_opt %t3 -o %t4
+// RUN: %sea_horn --solve %t4 | FileCheck %s
 
 extern void __VERIFIER_error (void);
 extern void __VERIFIER_assume (int);
@@ -15,20 +19,13 @@ int foo(_Array_ptr<int> p : count(n), int n)
   _Array_ptr<int> a : count(n) = p;
 
   a[n / 2] = 1;
-  // CHECK: _Dynamic_check.failed{{.*}}:
-  // CHECK-NEXT: call void @__VERIFIER_error()
-  // CHECK-NEXT: call void @llvm.trap()
-  // CHECK-NEXT: unreachable
 
   int i = 0, s = 0;
   for(i=0; i<n; i++)
     s += a[i];
-  // CHECK: _Dynamic_check.failed{{.*}}:
-  // CHECK-NEXT: call void @__VERIFIER_error()
-  // CHECK-NEXT: call void @llvm.trap()
-  // CHECK-NEXT: unreachable
 
   return s;
 }
 
+// CHECK: unsat
 
