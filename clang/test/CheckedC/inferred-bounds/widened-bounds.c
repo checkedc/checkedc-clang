de4d4ee2004ee531021418192c603f88335275df
Mandeep Singh Grang <magrang@microsoft.com>
2019-12-20 13:12:24 -0800

    Update 1

--- clang/test/CheckedC/inferred-bounds/widened-bounds.c
+++ clang/test/CheckedC/inferred-bounds/widened-bounds.c
@@ -54,88 +54,20 @@ void f3() {
 // CHECK-NOT: upper_bound(p) = 1
 }
 
-void f4(_Nt_array_ptr<char> p : count(0)) {
+void f4() {
+  _Nt_array_ptr<char> p : count(0) = "a";
+
   if (p[0]) {}
 
 // CHECK: In function: f4
-// CHECK: [B6]
-// CHECK:   1: p[0]
-// CHECK: [B5]
-// CHECK: upper_bound(p) = 1
-
-  _Nt_array_ptr<char> q : count(0) = "a";
-  if (0[q]) {}
-// CHECK: [B4]
-// CHECK:   2: 0[q]
-// CHECK: [B3]
-// CHECK: upper_bound(q) = 1
-
-  if ((((q[0])))) {}
-// CHECK: [B2]
-// CHECK:   1: (((q[0])))
-// CHECK: [B1]
-// CHECK: upper_bound(q) = 1
-}
-
-void f5() {
-  char p _Nt_checked[] : count(0) = "abc";
-
-  if (p[0])
-    if (p[1])   // expected-error {{out-of-bounds memory access}}
-      if (p[2]) // expected-error {{out-of-bounds memory access}}
-  {}
-
-// CHECK: In function: f5
 // CHECK: [B4]
 // CHECK:   2: p[0]
 // CHECK: [B3]
-// CHECK:   1: p[1]
-// CHECK: upper_bound(p) = 1
+// CHECK:upper_bound(p) = 1
+
+  if (0[p]) {}
 // CHECK: [B2]
-// CHECK:   1: p[2]
-// CHECK: upper_bound(p) = 2
+// CHECK:   1: 0[p]
 // CHECK: [B1]
-// CHECK: upper_bound(p) = 3
-}
-
-void f6(int i) {
-  char p _Nt_checked[] : bounds(p + i, p)  = "abc";
-
-  if (p[0]) {
-    i = 0;
-    if (p[1]) {}
-  }
-
-// CHECK: In function: f6
-// CHECK: [B3]
-// CHECK:   2: p[0]
-// CHECK: [B2]
-// CHECK:   1: i = 0
-// CHECK:   2: p[1]
-// CHECK: upper_bound(p) = 1
-// CHECK: [B1]
-// CHECK-NOT: upper_bound(p)
-}
-
-void f7(char p _Nt_checked[] : count(0)) {
-  if (p[0]) {}
-
-// CHECK: In function: f7
-// CHECK: [B6]
-// CHECK:   1: p[0]
-// CHECK: [B5]
-// CHECK: upper_bound(p) = 1
-
-  char q _Nt_checked[] : count(0) = "a";
-  if (0[q]) {}
-// CHECK: [B4]
-// CHECK:   2: 0[q]
-// CHECK: [B3]
-// CHECK: upper_bound(q) = 1
-
-  if ((((q[0])))) {}
-// CHECK: [B2]
-// CHECK:   1: (((q[0])))
-// CHECK: [B1]
-// CHECK: upper_bound(q) = 1
+// CHECK:upper_bound(p) = 1
 }
