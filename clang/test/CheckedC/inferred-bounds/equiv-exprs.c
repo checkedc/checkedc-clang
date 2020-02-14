af2171175ad30597b4ca9e403fde7de0f2c1d425
kakje <kakje@microsoft.com>
2020-02-13 16:05:37 -0800

    Add tests for cast expressions (which currently do not update the state)

--- clang/test/CheckedC/inferred-bounds/equiv-exprs.c
+++ clang/test/CheckedC/inferred-bounds/equiv-exprs.c
@@ -11,10 +11,13 @@
 extern int a1 [12];
 extern void g1(void);
 
+// Note: the expressions tested below also include implicit casts.
+// Bounds checking methods currently do not update equivalent
+// expression sets for cast expressions, so these sets after
+// checking a cast expression will be the same as the sets after
+// checking the cast's subexpression.
+
 // DeclRefExpr
-// Note: these expressions also include implicit LValueToRValue and
-// ArrayToPointerDecay casts.  These casts are not currently handled by
-// bounds checking methods.
 void f1(int i, int a checked[5]) {
   // Non-array, non-function type
   i;
@@ -25,7 +28,17 @@ void f1(int i, int a checked[5]) {
   // CHECK-NEXT: Expressions that produce the same value as S:
   // CHECK-NEXT: {
   // CHECK-NEXT: UnaryOperator {{.*}} '&'
-  // CHECK-NEXT: DeclRefExpr {{.*}} 'i'
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'i'
+  // CHECK-NEXT: }
+  // CHECK: Statement S:
+  // CHECK-NEXT: ImplicitCastExpr {{.*}} <LValueToRValue>
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'i'
+  // CHECK-NEXT: Sets of equivalent expressions after checking S:
+  // CHECK-NEXT: { }
+  // CHECK-NEXT: Expressions that produce the same value as S:
+  // CHECK-NEXT: {
+  // CHECK-NEXT: UnaryOperator {{.*}} '&'
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'i'
   // CHECK-NEXT: }
 
   // Function type
@@ -36,12 +49,28 @@ void f1(int i, int a checked[5]) {
   // CHECK-NEXT: { }
   // CHECK-NEXT: Expressions that produce the same value as S:
   // CHECK-NEXT: { }
+  // CHECK: Statement S:
+  // CHECK-NEXT: ImplicitCastExpr {{.*}} <FunctionToPointerDecay>
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'g1'
+  // CHECK-NEXT: Sets of equivalent expressions after checking S:
+  // CHECK-NEXT: { }
+  // CHECK-NEXT: Expressions that produce the same value as S:
+  // CHECK-NEXT: { }
 
   // Local checked array with known size
   int arr checked[10];
   arr;
   // CHECK: Statement S:
   // CHECK: DeclRefExpr {{.*}} 'arr'
+  // CHECK-NEXT: Sets of equivalent expressions after checking S:
+  // CHECK-NEXT: { }
+  // CHECK-NEXT: Expressions that produce the same value as S:
+  // CHECK-NEXT: {
+  // CHECK-NEXT: DeclRefExpr {{.*}} 'arr'
+  // CHECK-NEXT: }
+  // CHECK: Statement S:
+  // CHECK-NEXT: ImplicitCastExpr {{.*}} <ArrayToPointerDecay>
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'arr'
   // CHECK-NEXT: Sets of equivalent expressions after checking S:
   // CHECK-NEXT: { }
   // CHECK-NEXT: Expressions that produce the same value as S:
@@ -59,6 +88,15 @@ void f1(int i, int a checked[5]) {
   // CHECK-NEXT: {
   // CHECK-NEXT: DeclRefExpr {{.*}} 'a1'
   // CHECK-NEXT: }
+  // CHECK: Statement S:
+  // CHECK-NEXT: ImplicitCastExpr {{.*}} <ArrayToPointerDecay>
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'a1'
+  // CHECK-NEXT: Sets of equivalent expressions after checking S:
+  // CHECK-NEXT: { }
+  // CHECK-NEXT: Expressions that produce the same value as S:
+  // CHECK-NEXT: {
+  // CHECK-NEXT: DeclRefExpr {{.*}} 'a1'
+  // CHECK-NEXT: }
 
   // Array parameter with _Array_ptr<int> type
   a;
@@ -69,6 +107,16 @@ void f1(int i, int a checked[5]) {
   // CHECK-NEXT: Expressions that produce the same value as S:
   // CHECK-NEXT: {
   // CHECK-NEXT: UnaryOperator {{.*}} '&'
-  // CHECK-NEXT: DeclRefExpr {{.*}} 'a'
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'a'
+  // CHECK-NEXT: }
+  // CHECK: Statement S:
+  // CHECK-NEXT: ImplicitCastExpr {{.*}} <LValueToRValue>
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'a'
+  // CHECK-NEXT: Sets of equivalent expressions after checking S:
+  // CHECK-NEXT: { }
+  // CHECK-NEXT: Expressions that produce the same value as S:
+  // CHECK-NEXT: {
+  // CHECK-NEXT: UnaryOperator {{.*}} '&'
+  // CHECK-NEXT: `-DeclRefExpr {{.*}} 'a'
   // CHECK-NEXT: }
 }
