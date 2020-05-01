61b628b541d66d606c1c1131ddef11ff47b8a852
Aravind Machiry <machiry@cs.ucsb.edu>
2020-05-01 14:58:31 -0700

    Making varargs as WILD

--- clang/tools/checked-c-convert/ConstraintBuilder.cpp
+++ clang/tools/checked-c-convert/ConstraintBuilder.cpp
@@ -28,10 +28,11 @@ static
 void specialCaseVarIntros(ValueDecl *D, ProgramInfo &Info, ASTContext *C) {
   // Constrain everything that is void to wild.
   Constraints &CS = Info.getConstraints();
+  std::string TyStr = D->getType().getAsString();
 
   // Special-case for va_list, constrain to wild.
-  if (D->getType().getAsString() == "va_list" ||
-      D->getType()->isVoidType()) {
+  if (TyStr == "va_list" || TyStr == "struct __va_list_tag *" ||
+      TyStr == "struct __va_list_tag" || D->getType()->isVoidType()) {
     for (const auto &I : Info.getVariable(D, C))
       if (const PVConstraint *PVC = dyn_cast<PVConstraint>(I))
         for (const auto &J : PVC->getCvars())
