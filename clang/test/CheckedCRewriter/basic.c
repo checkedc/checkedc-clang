9fdedada6441b35d12f5e3e19b95144a02edbccf
Aravind Machiry <machiry@cs.ucsb.edu>
2020-05-05 02:33:15 -0700

    Fixing tests

--- clang/test/CheckedCRewriter/basic.c
+++ clang/test/CheckedCRewriter/basic.c
@@ -125,7 +125,7 @@ void basic_realloc(int count) {
 
     free(ptr);
 }
-//CHECK: int *ptr = (int*) calloc(n, sizeof(int));
+//CHECK: int *ptr = (int*) malloc(n1 * sizeof(int));
 
 struct student
 {
@@ -182,7 +182,7 @@ struct student * new_student() {
 }
 //CHECK: _Ptr<struct student> new_student(void) {
 //CHECK-NEXT: char name[] = "Bilbo Baggins";
-//CHECK: struct student *new_s = malloc(sizeof(struct student));
+//CHECK: _Ptr<struct student> new_s =  malloc(sizeof(struct student));
 
 int main() {
 	basic1();
