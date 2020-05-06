a820a8902427101d1b63fd116be8bd7e36fb3fe5
mwhicks1 <mwh@cs.umd.edu>
2020-05-06 15:24:59 -0400

    Testcase for new support for &

---
+++ clang/test/CheckedCRewriter/amper.c
@@ -0,0 +1,25 @@
+// RUN: cconv-standalone %s -- | FileCheck -match-full-lines %s
+
+void foo(int *x) {
+  x = (int *)5;
+  int **y = &x;
+}
+//CHECK:   _Ptr<int*> y =  &x;
+
+void bar(int *x) {
+  x = (int *)5;
+  int *y = *(&x);
+}
+
+int f(int *x) {
+  return *x;
+}
+//CHECK: int f(_Ptr<int> x) {
+
+void baz(void) {
+  int (*fp)(int *) = f;
+  int (*fp2)(int *) = &f;
+  f((void*)0);
+}
+//CHECK:  _Ptr<int (_Ptr<int> )> fp =  f;
+//CHECK:  _Ptr<int (_Ptr<int> )> fp2 =  &f;
