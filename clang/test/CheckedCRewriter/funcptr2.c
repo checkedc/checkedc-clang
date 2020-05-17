64ab48525ed9d95936fe3a0f70e45fa493125c93
Aravind Machiry <machiry@cs.ucsb.edu>
2020-05-17 15:09:34 -0700

    Adding function pointer tests

---
+++ clang/test/CheckedCRewriter/funcptr2.c
@@ -0,0 +1,20 @@
+// RUN: cconv-standalone %s -- | FileCheck -match-full-lines %s
+//
+
+void f(int *(*fp)(int *)) {
+  int *x = (int *)5;
+  int *z = (int *)5;
+  z = fp(x);
+}
+//CHECK: void f(_Ptr<int* (int *)> fp) {
+int *g2(int *x) {
+  return x;
+}
+int *g(int *x) {
+  x = (int *)5;
+  return 0;
+}
+void h() {
+  f(g);
+  f(g2);
+}
---
+++ clang/test/CheckedCRewriter/funcptr4.c
@@ -0,0 +1,21 @@
+// RUN: cconv-standalone %s -- | FileCheck -match-full-lines %s
+//
+void f(int *(*fp)(int *)) {
+  fp(0);
+}
+//CHECK: void f(_Ptr<_Ptr<int> (_Ptr<int> )> fp) {
+int *g2(int *x) {
+  return x;
+}
+//CHECK: _Ptr<int> g2(_Ptr<int> x) {
+int *g(int *x) {
+  return 0;
+}
+//CHECK: _Ptr<int> g(_Ptr<int> x) {
+void h() {
+  int *(*fp)(int *) = g;
+  f(g);
+  f(g2);
+  g(0);
+}
+//CHECK: _Ptr<_Ptr<int> (_Ptr<int> )> fp =  g;
