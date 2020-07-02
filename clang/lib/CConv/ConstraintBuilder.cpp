07f3054e86a208bb1c49fb346a967d2f4946068d
Shilpa Roy <shilparoy4899@gmail.com>
2020-07-02 16:02:16 -0400

    made BeginLoc and EndLoc unsigned integers

--- clang/lib/CConv/ConstraintBuilder.cpp
+++ clang/lib/CConv/ConstraintBuilder.cpp
@@ -350,8 +350,8 @@ public:
       // If the location of the previous RecordDecl and the current VarDecl
       // coincide with one another, we constrain the VarDecl to be wild
       // in order to allow the fields of the RecordDecl to be converted
-      int BeginLoc = G->getBeginLoc().getRawEncoding();
-      int EndLoc = G->getEndLoc().getRawEncoding();
+      unsigned int BeginLoc = G->getBeginLoc().getRawEncoding();
+      unsigned int EndLoc = G->getEndLoc().getRawEncoding();
       if (lastRecordLocation >= BeginLoc && lastRecordLocation <= EndLoc) {
         std::set<ConstraintVariable *> C = Info.getVariable(G, Context);
         CB.constraintAllCVarsToWild(C, "Inline struct encountered.", nullptr);
