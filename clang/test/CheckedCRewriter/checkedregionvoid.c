173da80110d3289158c6b88294161c4e706f573d
Aaron Eline <aaron@correctcomputation.com>
2020-07-15 22:55:11 -0400

    Added testcase for bug

---
+++ clang/test/CheckedCRewriter/checkedregionvoid.c
@@ -0,0 +1,9 @@
+// RUN: cconv-standalone -addcr  %s -- | FileCheck -match-full-lines --check-prefixes="CHECK" %s
+#include "dummy.h"
+// Dummy to cause output
+void f(int *x) {}
+
+void foo(char c) { 
+  unsafe(&c);
+}
+
