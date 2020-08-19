7cd1f522643483d954cfe1011815aa83d67d0bbb
Shilpa Roy <shilparoy4899@gmail.com>
2020-08-19 14:02:12 -0400

    spruce up anonstruct.c

--- clang/test/CheckedCRewriter/anonstruct.c
+++ clang/test/CheckedCRewriter/anonstruct.c
@@ -6,13 +6,18 @@
 // RUN: rm %S/anonstruct.checked.c
 
 struct { 
+	/*the fields of the anonymous struct are free to be marked checked*/
     int *data; 
-	//CHECK: _Ptr<int> data; 
+	//CHECK_NOALL: int *data; 
+	//CHECK_ALL: _Array_ptr<int> data : count(4); 
+
+/* but the actual pointer can't be */
 } *x; 
 
 /*ensure trivial conversion*/
-void foo(int *x) { 
-	//CHECK: void foo(_Ptr<int> x) _Checked { 
-
+void foo(int *w) { 
+	//CHECK: void foo(_Ptr<int> w) { 
+	x->data = malloc(sizeof(int)*4); 
+	x->data[1] = 4;
 } 
 
