6087a72b5cf1e2e62c8a9ba46256a2f9e3382ff7
Mandeep Singh Grang <magrang@microsoft.com>
2020-11-16 16:05:41 -0800

    Compare Expressions: Remove erroneous condition on coalescing nodes
    We had erroneously added the condition that to coalesce nodes the binary
    operators of the parent and child node must be equal. This fails for the case
    where we first constant-fold nodes having a differen operator than the parent
    and then need to coalesce the constant folded node into the parent.
    
    Fixes issue #932

--- clang/lib/AST/PreorderAST.cpp
+++ clang/lib/AST/PreorderAST.cpp
@@ -42,14 +42,10 @@ void PreorderAST::RemoveNode(Node *N, Node *Parent) {
   // We will remove a BinaryNode only if its operator is equal to its
   // parent's operator and the operator is commutative and associative.
   if (auto *B = dyn_cast<BinaryNode>(N)) {
-    assert(B->Opc == P->Opc &&
-           "BinaryNode operator must equal parent operator");
-
     assert(B->IsOpCommutativeAndAssociative() &&
            "BinaryNode operator must be commutative and associative");
 
-    if (B->Opc != P->Opc ||
-       !B->IsOpCommutativeAndAssociative()) {
+    if (!B->IsOpCommutativeAndAssociative()) {
       SetError();
       return;
     }
