c84741b937cfff88bba48b96f118383e84a2dcf7
Mandeep Singh Grang <magrang@microsoft.com>
2020-12-04 17:00:05 -0800

    Disable 3C tests on Windows
    These tests are failing on Windows. Disabling them temporarily to unblock
    Windows builds.

--- clang/test/3C/3d-allocation.c
+++ clang/test/3C/3d-allocation.c
@@ -1,3 +1,4 @@
+// UNSUPPORTED: system-windows
 // RUN: 3c -alltypes -addcr %s -- | FileCheck -match-full-lines -check-prefixes="CHECK_ALL","CHECK" %s
 // RUN: 3c -addcr %s -- | FileCheck -match-full-lines -check-prefixes="CHECK_NOALL","CHECK" %s
 // RUN: 3c -addcr %s -- | %clang -c -fcheckedc-extension -x c -o /dev/null -
