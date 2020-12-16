d8682772142b5737f9b6d0e7e8fa3df138acb560
kakje <kakje@microsoft.com>
2020-12-15 18:02:35 -0800

    Fix build failures in SimpleBoundsChecker.cpp

--- clang/lib/StaticAnalyzer/Checkers/SimpleBoundsChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/SimpleBoundsChecker.cpp
@@ -32,6 +32,7 @@
 #include "clang/StaticAnalyzer/Core/Checker.h"
 #include "clang/StaticAnalyzer/Core/CheckerManager.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/DynamicSize.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/ExprEngine.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/SMTConv.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/SMTAPI.h"
@@ -103,9 +104,10 @@ void SimpleBoundsChecker::checkLocation(SVal l, bool isLoad, const Stmt *LoadS,
 
   // Get the size of the array.
   DefinedOrUnknownSVal NumElements =
-      C.getStoreManager().getSizeInElements(State,
-                                            ER->getSuperRegion(),
-                                            ER->getValueType());
+      getDynamicElementCount(State,
+                             ER->getSuperRegion(),
+                             SvalBuilder,
+                             ER->getValueType());
 
   ProgramStateRef StInBound = State->assumeInBound(Idx, NumElements, true);
   ProgramStateRef StOutBound = State->assumeInBound(Idx, NumElements, false);
@@ -263,11 +265,12 @@ void SimpleBoundsChecker::reportOutofBoundsAccess(ProgramStateRef OutBound,
           this, "Out-of-bound array access",
           "Access out-of-bound array element (buffer overflow)"));
 
+  // TODO: checkedc-clang issue #931: uncomment below to generate a BugReport.
   // Generate a report for this bug.
-  auto Report = llvm::make_unique<BugReport>(*BT, BT->getDescription(), N);
+  // auto Report = std::make_unique<BugReport>(*BT, BT->getDescription(), N);
 
-  Report->addRange(LoadS->getSourceRange());
-  C.emitReport(std::move(Report));
+  // Report->addRange(LoadS->getSourceRange());
+  // C.emitReport(std::move(Report));
   return;
 }
 
@@ -370,6 +373,6 @@ void ento::registerSimpleBoundsChecker(CheckerManager &mgr) {
 }
 
 // This checker should be enabled regardless of how language options are set.
-bool ento::shouldRegisterSimpleBoundsChecker(const LangOptions &LO) {
+bool ento::shouldRegisterSimpleBoundsChecker(const CheckerManager &mgr) {
   return true;
 }
