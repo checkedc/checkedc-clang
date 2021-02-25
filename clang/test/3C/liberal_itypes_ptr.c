54d1bfd92d52b637a6c06d7297a8e8f0cd524b3a
Matt McCutchen (Correct Computation) <matt@correctcomputation.com>
2021-02-25 16:40:18 -0500

    3C regression tests: Replace remaining size_t typedefs with stddef.h. (#453)
    The one in liberal_itypes_ptr.c conflicted with the system headers on
    Windows X64. We haven't seen a problem with the one in allocator.c yet,
    but let's remove it too for consistency.
    
    Fixes #451.

--- clang/test/3C/liberal_itypes_ptr.c
+++ clang/test/3C/liberal_itypes_ptr.c
@@ -41,7 +41,7 @@ void biz() {
   // CHECK: b = buz();
 }
 
-typedef unsigned long size_t;
+#include <stddef.h>
 _Itype_for_any(T) void *malloc(size_t size) : itype(_Array_ptr<T>) byte_count(size);
 _Itype_for_any(T) void free(void *pointer : itype(_Array_ptr<T>) byte_count(0));
 
