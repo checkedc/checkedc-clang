21fd6b49e08af1d12b286c64b559c3cfed14bc49
Mandeep Singh Grang <magrang@microsoft.com>
2021-03-01 22:37:56 -0800

    [Where Clause] Introduce WhereClauseScope to emit better diagnostics [2/n]
    We introduce a new scope for where clauses. Before parsing a where clause we
    enter the WhereClauseScope and at the end of parsing we exit the scope. In Sema
    we check if we are in the WhereClauseScope and emit the relevant diagnostics.

--- clang/test/CheckedC/parsing/invalid-where-clause.c
+++ clang/test/CheckedC/parsing/invalid-where-clause.c
@@ -5,19 +5,19 @@
 int f();
 
 void invalid_cases(_Nt_array_ptr<char> p, int a, int b) {
-  _Where ; // expected-error {{expected expression}}
-  _Where ;;;;; // expected-error {{expected expression}}
-  _Where _Where; // expected-error {{expected expression}} expected-error {{expected expression}}
-  _Where _And; // expected-error {{expected expression}} expected-error {{expected expression}}
-  _Where _And _And _And; // expected-error {{expected expression}} expected-error {{expected expression}} expected-error {{expected expression}} expected-error {{expected expression}}
+  _Where ; // expected-error {{expected bounds declaration or equality expression in where clause}}
+  _Where ;;;;; // expected-error {{expected bounds declaration or equality expression in where clause}}
+  _Where _Where; // expected-error {{expected bounds declaration or equality expression in where clause}} expected-error {{expected bounds declaration or equality expression in where clause}}
+  _Where _And; // expected-error {{expected bounds declaration or equality expression in where clause}} expected-error {{expected bounds declaration or equality expression in where clause}}
+  _Where _And _And _And; // expected-error {{expected bounds declaration or equality expression in where clause}} expected-error {{expected bounds declaration or equality expression in where clause}} expected-error {{expected bounds declaration or equality expression in where clause}} expected-error {{expected bounds declaration or equality expression in where clause}}
   _Where a; // expected-error {{expected comparison operator in equality expression}}
   _Where a _Where a _Where a; // expected-error {{expected comparison operator in equality expression}} expected-error {{expected comparison operator in equality expression}} expected-error {{expected comparison operator in equality expression}}
-  _Where a _And; // expected-error {{expected comparison operator in equality expression}} expected-error {{expected expression}}
+  _Where a _And; // expected-error {{expected comparison operator in equality expression}} expected-error {{expected bounds declaration or equality expression in where clause}}
   _Where x; // expected-error {{use of undeclared identifier 'x'}}
   _Where p : ; // expected-error {{expected bounds expression}}
   _Where p : count(x); // expected-error {{use of undeclared identifier 'x'}}
   _Where q : count(0); // expected-error {{use of undeclared identifier q}}
-  _Where (); // expected-error {{expected expression}}
+  _Where (); // expected-error {{expected bounds declaration or equality expression in where clause}}
   _Where a = 0; // expected-error {{expected comparison operator in equality expression}}
   _Where a == 1 _And a; // expected-error {{expected comparison operator in equality expression}}
   _Where a _And a == 1; // expected-error {{expected comparison operator in equality expression}}
@@ -34,5 +34,5 @@ void invalid_cases(_Nt_array_ptr<char> p, int a, int b) {
   _Where f() == 1; // expected-error {{call expression not allowed in expression}}
   _Where 1 != f(); // expected-error {{call expression not allowed in expression}}
   _Where a++ < 1; // expected-error {{increment expression not allowed in expression}}
-  _Where a _And p : _And q : count(0) _And x _And a = 1 _And f() < 0 _And; // expected-error {{expected comparison operator in equality expression}} expected-error {{expected bounds expression}} expected-error {{use of undeclared identifier q}} expected-error {{use of undeclared identifier 'x'}} expected-error {{expected comparison operator in equality expression}} expected-error {{call expression not allowed in expression}} expected-error {{expected expression}}
+  _Where a _And p : _And q : count(0) _And x _And a = 1 _And f() < 0 _And; // expected-error {{expected comparison operator in equality expression}} expected-error {{expected bounds expression}} expected-error {{use of undeclared identifier q}} expected-error {{use of undeclared identifier 'x'}} expected-error {{expected comparison operator in equality expression}} expected-error {{call expression not allowed in expression}} expected-error {{expected bounds declaration or equality expression in where clause}}
 }
