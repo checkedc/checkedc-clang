da8c3d73052ee9b071198b3637424afdc259a4a4
Mandeep Singh Grang <magrang@microsoft.com>
2021-04-12 12:30:25 -0700

    [Where Clause] Dataflow analysis for bounds widening with where clauses [5/n]
    This PR adds the interface for the dataflow analysis for bounds widening of
    null-terminated arrays, in presence of where clauses. We declare types, classes
    and functions used by the analysis.
    
    We declare the BoundsWideningAnalysis class that represents the dataflow
    analysis for bounds widening. The sets In, Out, Gen and Kill that are used by
    the analysis are members of this class. The class also has methods that act on
    these sets to perform the dataflow analysis.

---
+++ clang/lib/Sema/BoundsWideningAnalysis.cpp
@@ -0,0 +1,16 @@
+//===== BoundsWideningAnalysis.h - Dataflow analysis for bounds widening ====//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===---------------------------------------------------------------------===//
+//  This file implements a dataflow analysis for bounds widening.
+//===---------------------------------------------------------------------===//
+
+#include "clang/Sema/BoundsWideningAnalysis.h"
+
+namespace clang {
+} // end namespace clang
