3c356f8c7a3a623d413074fa0a1bab15c257cbd1
Mandeep Singh Grang <magrang@microsoft.com>
2021-04-14 15:56:21 -0700

    [BoundsWidening] Class & type definitions for dataflow analysis for bounds widening [1/n] (#1023)
    This PR adds the interface for the dataflow analysis for bounds widening of
    null-terminated arrays, in presence of where clauses. We declare types, classes
    and functions used by the analysis.
    
    We declare the BoundsWideningAnalysis class that represents the dataflow
    analysis for bounds widening. The sets In, Out, Gen and Kill that are used by
    the analysis are members of this class. The class also has methods that act on
    these sets to perform the dataflow analysis.

---
+++ clang/lib/Sema/BoundsWideningAnalysis.cpp
@@ -0,0 +1,17 @@
+//===== BoundsWideningAnalysis.h - Dataflow analysis for bounds widening ====//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===---------------------------------------------------------------------===//
+// This file implements a dataflow analysis for bounds widening as described in
+// https://github.com/microsoft/checkedc-clang/blob/master/clang/docs/checkedc/Bounds-Widening-for-Null-Terminated-Arrays.md
+//===---------------------------------------------------------------------===//
+
+#include "clang/Sema/BoundsWideningAnalysis.h"
+
+namespace clang {
+} // end namespace clang
