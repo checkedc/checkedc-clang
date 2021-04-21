f18cdc360aa36cc9a9c5807ab34f56fdab9ddaec
kakje <kakje@microsoft.com>
2021-04-21 15:16:03 -0700

    Add header with license information to AbstractSet files

--- clang/include/clang/AST/AbstractSet.h
+++ clang/include/clang/AST/AbstractSet.h
@@ -1,3 +1,18 @@
+//===------- AbstractSet.h: An abstract representation of memory -------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===-------------------------------------------------------------------===//
+//
+//  This file defines the interface for a representation of a set of memory
+//  locations.
+//
+//===-------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_ABSTRACT_SET_H
 #define LLVM_CLANG_ABSTRACT_SET_H
 
--- clang/lib/AST/AbstractSet.cpp
+++ clang/lib/AST/AbstractSet.cpp
@@ -1,3 +1,18 @@
+//===------- AbstractSet.cpp: An abstract representation of memory -------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===---------------------------------------------------------------------===//
+//
+//  This file implements methods to get or create a representation of a set of
+//  memory locations.
+//
+//===---------------------------------------------------------------------===//
+
 #include "clang/AST/AbstractSet.h"
 #include "clang/AST/ExprUtils.h"
 
