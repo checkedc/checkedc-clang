f60113f92d191e880013c86c55ceec4c2e120731
kakje <kakje@microsoft.com>
2021-07-07 15:53:39 -0700

    Add NormalizeUtils.h and NormalizeUtils.cpp

---
+++ clang/include/clang/AST/NormalizeUtils.h
@@ -0,0 +1,28 @@
+//===----------- NormalizeUtils.h: Functions for normalizing expressions --===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file defines the functions for normalizing expressions.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_NORMALIZEUTILS_H
+#define LLVM_CLANG_NORMALIZEUTILS_H
+
+#include "clang/AST/Expr.h"
+#include "clang/Sema/Sema.h"
+
+namespace clang {
+
+class NormalizeUtil {
+
+};
+
+} // end namespace clang
+#endif
---
+++ clang/lib/AST/NormalizeUtils.cpp
@@ -0,0 +1,17 @@
+//===--------- NormalizeUtils.cpp: Functions for normalizing expressions --===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file implements functions for normalizing expressions.
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/AST/NormalizeUtils.h"
+
+using namespace clang;
