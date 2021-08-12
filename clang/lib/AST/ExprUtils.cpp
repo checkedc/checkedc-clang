35fbbde3fe738527c933d7ce8b3311f8726630a0
Mandeep Singh Grang <magrang@microsoft.com>
2021-08-12 11:24:53 -0700

    Verify if an expr contains errors before checking invertibility  (#1154)
    The community has introduced a new annotation called "contains-errors" on AST
    nodes that contain semantic errors. As a result, after the upgrade of Checked C
    sources to LLVM 12 we need to check if an expr contains errors before operating
    on the expr. One such place is in InverseUtil::IsInvertible where we need to
    check if the input modifying expr contains errors.
    
    * Added containsErrors checks to InverUtil::Inverse

--- clang/lib/AST/ExprUtils.cpp
+++ clang/lib/AST/ExprUtils.cpp
@@ -469,7 +469,7 @@ void ExprUtil::EnsureEqualBitWidths(llvm::APSInt &A, llvm::APSInt &B) {
 }
 
 bool InverseUtil::IsInvertible(Sema &S, Expr *LValue, Expr *E) {
-  if (!E)
+  if (!E || E->containsErrors())
     return false;
 
   E = E->IgnoreParens();
@@ -628,7 +628,10 @@ bool InverseUtil::IsCastExprInvertible(Sema &S, Expr *LValue, CastExpr *E) {
 }
 
 Expr *InverseUtil::Inverse(Sema &S, Expr *LValue, Expr *F, Expr *E) {
-  if (!F)
+  if (!F || F->containsErrors())
+    return nullptr;
+
+  if (!E || E->containsErrors())
     return nullptr;
 
   E = E->IgnoreParens();
