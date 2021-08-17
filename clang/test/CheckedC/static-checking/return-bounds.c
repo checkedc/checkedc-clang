86ccdbf9a795517bfc4560deb0717bb34a85fff6
kakje <kakje@microsoft.com>
2021-08-17 13:53:10 -0700

    Update return bounds tests (add more tests for bounds-safe interfaces)

--- clang/test/CheckedC/static-checking/return-bounds.c
+++ clang/test/CheckedC/static-checking/return-bounds.c
@@ -86,7 +86,7 @@ _Array_ptr<int> f12(_Array_ptr<int> p : count(2)) : bounds(p - 1, p + 2) { // ex
 
 _Nt_array_ptr<int> f13(_Nt_array_ptr<int> p, // expected-note 2 {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 3)'}}
                        _Nt_array_ptr<int> q : bounds(q + 1, q + 2),
-                       int test) : count(3) {
+                       int test) : count(3) _Unchecked {
   if (test)
     return p; // expected-error {{return value bounds do not imply declared return bounds for 'f13'}} \
               // expected-note {{source bounds are an empty range}} \
@@ -130,40 +130,70 @@ _Nt_array_ptr<char> f16(int i) : count(i) { // expected-note {{(expanded) declar
 // Test bounds-safe interfaces
 //
 
-int *f17(int *p) : byte_count(4) _Unchecked { // expected-note {{(expanded) declared return bounds are 'bounds((_Array_ptr<char>)_Return_value, (_Array_ptr<char>)_Return_value + 4)'}}
-  return p; // expected-error {{return value has unknown bounds, bounds expected because the function 'f17' has bounds}}
+int *f17(int p[2]) : byte_count(4) _Unchecked {
+  return p;
 }
 
-int *f18(int *p : bounds(unknown)) : count(5) _Checked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 5)'}}
-  return p; // expected-error {{return value has unknown bounds, bounds expected because the function 'f18' has bounds}}
+int *f18(int *p : count(2)) : count(5) _Unchecked {
+  return p;
 }
 
-int *f19(int *p : count(6)) : count(6) _Unchecked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 6)'}}
-  return p + 1; // expected-warning {{cannot prove return value bounds imply declared return bounds for 'f19'}} \
-                // expected-note {{(expanded) inferred return value bounds are 'bounds(p, p + 6)'}}
+int *f19(int *p : count(i), unsigned int i) : count(i + 1) _Unchecked {
+  return (p);
 }
 
-int *f20(int *p : count(7)) : bounds(p, p + 7) _Checked {
+int *f20(_Array_ptr<int> p : bounds(unknown)) : count(6) _Unchecked {
+  return (int *)p;
+}
+
+int *f21(int *p : count(7)) : count(7) _Unchecked {
   return p + 1;
 }
 
-int *f21(_Array_ptr<int> p : count(8)) : count(8) _Unchecked {
-  return p;
+int *f22(_Array_ptr<int> p) : count(8) _Unchecked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 8)'}}
+  return p; // expected-error {{return value has unknown bounds, bounds expected because the function 'f22' has bounds}}
+}
+
+int *f23(int *p : count(1)) : count(9) _Unchecked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 9)'}}
+  return (_Array_ptr<int>)p; // expected-error {{return value bounds do not imply declared return bounds for 'f23'}} \
+                             // expected-note {{declared return bounds are wider than the return value bounds}} \
+                             // expected-note {{declared return upper bound is above return value upper bound}} \
+                             // expected-note {{(expanded) inferred return value bounds are 'bounds(p, p + 1)'}}
+}
+
+int *f24(int *p : bounds(unknown)) : count(4) _Checked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 4)'}}
+  return p; // expected-error {{return value has unknown bounds, bounds expected because the function 'f24' has bounds}}
+}
+
+int *f25(int *p : count(5)) : bounds(p, p + 5) _Checked {
+  return p + 1;
+}
+
+int *f26(int *p : itype(_Array_ptr<int>), int test) : count(6) _Checked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + 6)'}}
+  if (test)
+    return p; // expected-error {{return value has unknown bounds, bounds expected because the function 'f26' has bounds}}
+
+  _Unchecked { return p; }
+}
+
+_Array_ptr<int> f27(int *p : count(i), int i) : count(i + 1) _Checked { // expected-note {{(expanded) declared return bounds are 'bounds(_Return_value, _Return_value + i + 1)'}}
+  return p; // expected-warning {{cannot prove return value bounds imply declared return bounds for 'f27'}} \
+            // expected-note {{(expanded) inferred return value bounds are 'bounds(p, p + i)'}}
 }
 
 //
 // Test bounds casts
 //
 
-_Array_ptr<char> f22(_Array_ptr<char> p : count(i), int i) : count(3) {
+_Array_ptr<char> f28(_Array_ptr<char> p : count(i), int i) : count(3) {
   return _Dynamic_bounds_cast<_Array_ptr<char>>(p, count(3));
 }
 
-_Array_ptr<int> f23(_Array_ptr<int> p : count(4), int i) : count(i) {
+_Array_ptr<int> f29(_Array_ptr<int> p : count(4), int i) : count(i) {
   return _Assume_bounds_cast<_Array_ptr<int>>(p, count(i));
 }
 
-_Array_ptr<int> f24(_Array_ptr<int> p : count(i), int i) : count(2) {
+_Array_ptr<int> f30(_Array_ptr<int> p : count(i), int i) : count(2) {
   return _Dynamic_bounds_cast<_Array_ptr<int>>(p + 1, count(2));
 }
 
@@ -175,14 +205,14 @@ extern _Array_ptr<int> g1(void) : count(2);
 extern int *g2(int size) : count(size);
 extern _Nt_array_ptr<char> g3(_Nt_array_ptr<char> p) : bounds(p, p);
 
-_Array_ptr<int> f25(void) : count(2) {
+_Array_ptr<int> f31(void) : count(2) {
   return g1();
 }
 
-_Array_ptr<int> f26(int len) : count(len + 1) {
+_Array_ptr<int> f32(int len) : count(len + 1) {
   return g2(len + 1);
 }
 
-_Nt_array_ptr<char> f27(_Nt_array_ptr<char> p) : bounds(p, p) {
+_Nt_array_ptr<char> f33(_Nt_array_ptr<char> p) : bounds(p, p) {
   return g3(p);
 }
