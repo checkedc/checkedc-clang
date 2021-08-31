99176efb9c3c9f773b88cd623c374cb9979645bd
Katherine Kjeer <6687333+kkjeer@users.noreply.github.com>
2021-08-30 19:12:39 -0700

    Update bounds checking notes (#1166)
    * Return the Bounds argument from ReplaceLValueInBounds if Bounds is unknown or any
    
    * Only store the first expression with unknown bounds assigned to an lvalue in A
    
    Only update BlameAssignments and UnknownSrcBounds if the assignment actually changed the bounds of LValueAbstractSet
    
    * Remove expected note about the expression 'b' with unknown bounds assigned to 'a'
    
    * Add test to bounds-decl-checking.c testing notes for multiple assignments
    
    * Add tests for bounds warnings

--- clang/test/CheckedC/static-checking/bounds-decl-checking.c
+++ clang/test/CheckedC/static-checking/bounds-decl-checking.c
@@ -818,6 +818,35 @@ void f96(_Nt_array_ptr<char> p : bounds(p, (p + (len + 4)) + 4), int len) {
 }
 
 //
+// Test diagnostic behavior for bounds checking across multiple assignments.
+//
+
+void f97(_Array_ptr<int> p : count(i), // expected-note 4 {{(expanded) declared bounds are 'bounds(p, p + i)'}}
+         unsigned int i, 
+         _Array_ptr<int> unknwn1,
+         _Array_ptr<int> unknwn2) {
+  p = unknwn1, p = p; // expected-error {{inferred bounds for 'p' are unknown after assignment}} \
+                      // expected-note {{assigned expression 'unknwn1' with unknown bounds to 'p'}}
+
+  p = unknwn1, p++; // expected-error {{inferred bounds for 'p' are unknown after assignment}} \
+                    // expected-note {{assigned expression 'unknwn1' with unknown bounds to 'p'}}
+
+  p = unknwn1, p = unknwn2; // expected-error {{inferred bounds for 'p' are unknown after assignment}} \
+                            // expected-note {{assigned expression 'unknwn1' with unknown bounds to 'p'}}
+
+  p++, i = 0, p = unknwn1; // expected-error {{inferred bounds for 'p' are unknown after assignment}} \
+                           // expected-note {{lost the value of the expression 'i' which is used in the (expanded) inferred bounds 'bounds(p - 1, p - 1 + i)' of 'p'}}
+}
+
+void f98(_Array_ptr<int> p : count(i), unsigned int i) { // expected-note 2 {{(expanded) declared bounds are 'bounds(p, p + i)'}}
+  i++, p += 2; // expected-warning {{cannot prove declared bounds for 'p' are valid after assignment}} \
+               // expected-note {{(expanded) inferred bounds are 'bounds(p - 2, p - 2 + i)'}}
+
+  p += 2, i++; // expected-warning {{cannot prove declared bounds for 'p' are valid after increment}} \
+               // expected-note {{(expanded) inferred bounds are 'bounds(p - 2, p - 2 + i - 1U)'}}
+}
+
+//
 // Test validating bounds for unchecked pointer with bounds-safe interfaces
 // in unchecked and checked scopes.
 //
@@ -825,7 +854,7 @@ void f96(_Nt_array_ptr<char> p : bounds(p, (p + (len + 4)) + 4), int len) {
 _Unchecked extern int *get_unchecked(_Array_ptr<int> a);
 extern _Array_ptr<int> get_checked(unsigned int i) : count(i);
 
-_Unchecked void f97(int *p : count(i), // expected-note 8 {{(expanded) declared bounds are 'bounds(p, p + i)'}}
+_Unchecked void f99(int *p : count(i), // expected-note 8 {{(expanded) declared bounds are 'bounds(p, p + i)'}}
                     int *q : bounds(unknown),
                     _Array_ptr<int> r,
                     unsigned int i) {
